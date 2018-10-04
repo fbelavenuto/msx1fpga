ea8ac8480cbd381795a43c20d8469fbd8aee14eb
Fabio Belavenuto <belavenuto@gmail.com>
2018-10-04 16:04:02 -0300

    Updated NEXTOR

--- Software/nextor/_make.cmd
+++ Software/nextor/_make.cmd
@@ -3,9 +3,9 @@ sjasmplus --lst=Driver.lst Driver.asm
 IF ERRORLEVEL 1 GOTO error
 
 rem mknexrom Nextor-2.1-alpha2.base.dat NEXTOR.ROM /d:driver.bin /m:Mapper.ASCII16.bin
-mknexrom Nextor-2.0.5-beta1.base.dat NEXTOR.ROM /d:driver.bin /m:Mapper.ASCII16.bin
+mknexrom Nextor-2.0.5-beta1.base.dat NEXTORH.ROM /d:driver.bin /m:Mapper.ASCII16.bin
 IF ERRORLEVEL 1 GOTO error
-copy NEXTOR.ROM ..\..\SD\MSX1FPGA
+copy NEXTORH.ROM ..\..\SD\MSX1FPGA
 goto ok
 
 :error
