ff7d7c3800390965d9a3151ea7a89a519237e4f0
Fabio Belavenuto <belavenuto@gmail.com>
2018-12-16 11:24:17 -0200

    Updating Nextor do use EMUFILE Fixing SPA keymap

--- Software/nextor/_make.cmd
+++ Software/nextor/_make.cmd
@@ -3,7 +3,8 @@ echo Making no HWDS
 sjasmplus --nologo -DHWDS=0 --lst=Driver.lst Driver.asm
 IF ERRORLEVEL 1 GOTO error
 
-mknexrom Nextor-2.0.5-beta1.base.dat NEXTOR.ROM /d:driver.bin /m:Mapper.ASCII16.bin
+rem mknexrom Nextor-2.0.5-beta1.base.dat NEXTOR.ROM /d:driver.bin /m:Mapper.ASCII16.bin
+mknexrom Nextor-2.1-alpha2.base.dat NEXTOR.ROM /d:driver.bin /m:Mapper.ASCII16.bin
 IF ERRORLEVEL 1 GOTO error
 copy NEXTOR.ROM ..\..\Support\SD\MSX1FPGA
 
@@ -11,7 +12,8 @@ echo Making HWDS
 sjasmplus --nologo -DHWDS=1 --lst=Driver.lst Driver.asm
 IF ERRORLEVEL 1 GOTO error
 
-mknexrom Nextor-2.0.5-beta1.base.dat NEXTORH.ROM /d:driver.bin /m:Mapper.ASCII16.bin
+rem mknexrom Nextor-2.0.5-beta1.base.dat NEXTORH.ROM /d:driver.bin /m:Mapper.ASCII16.bin
+mknexrom Nextor-2.1-alpha2.base.dat NEXTORH.ROM /d:driver.bin /m:Mapper.ASCII16.bin
 IF ERRORLEVEL 1 GOTO error
 copy NEXTORH.ROM ..\..\Support\SD\MSX1FPGA
 goto ok
