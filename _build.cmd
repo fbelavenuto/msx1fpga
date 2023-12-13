e30dd8449e2b15d5043ac3c72c783c52e6b94bac
Fabio Belavenuto <belavenuto@gmail.com>
2023-12-13 11:57:16 -0300

    Adding code to compile FPGAs

--- _build.cmd
+++ _build.cmd
@@ -3,13 +3,18 @@
 echo Updating docker image
 docker pull fbelavenuto/8bitcompilers
 docker pull fbelavenuto/xilinxise
+docker pull fbelavenuto/alteraquartus
 
 echo Building software
 docker run --rm -it -e TZ=America/Sao_Paulo -v %cd%:/src fbelavenuto/8bitcompilers make -f Makefile-software
 IF ERRORLEVEL 1 GOTO error
 
-echo Building FPGA bitstreams
-docker run --rm -it --mac-address 08:00:27:68:c9:35 -e TZ=America/Sao_Paulo -v %cd%:/workdir fbelavenuto/xilinxise make -f Makefile-fpga
+echo Building Xilinx FPGA bitstreams
+docker run --rm -it --mac-address 08:00:27:68:c9:35 -e TZ=America/Sao_Paulo -v %cd%:/workdir fbelavenuto/xilinxise make -f Makefile-xilinx
+IF ERRORLEVEL 1 GOTO error
+
+echo Building Altera FPGA bitstreams
+docker run --rm -it --mac-address 00:01:02:03:04:05 -e TZ=America/Sao_Paulo -v %cd%:/workdir fbelavenuto/alteraquartus make -f Makefile-altera
 IF ERRORLEVEL 1 GOTO error
 
 goto ok
--- _clean.cmd
+++ _clean.cmd
@@ -3,6 +3,7 @@
 echo Updating docker image
 docker pull fbelavenuto/8bitcompilers
 docker pull fbelavenuto/xilinxise
+docker pull fbelavenuto/alteraquartus
 
 echo Erasing unwanted files
 wsl find -type f -size 0 -delete 
@@ -14,8 +15,12 @@ echo Cleaning software
 docker run --rm -it -e TZ=America/Sao_Paulo -v %cd%:/src fbelavenuto/8bitcompilers make -f Makefile-software clean
 IF ERRORLEVEL 1 GOTO error
 
-echo Cleaning FPGA bitstreams
-docker run --rm -it --mac-address 08:00:27:68:c9:35 -e TZ=America/Sao_Paulo -v %cd%:/workdir fbelavenuto/xilinxise make -f Makefile-fpga clean
+echo Cleaning Xilinx bitstreams
+docker run --rm -it --mac-address 08:00:27:68:c9:35 -e TZ=America/Sao_Paulo -v %cd%:/workdir fbelavenuto/xilinxise make -f Makefile-xilinx clean
+IF ERRORLEVEL 1 GOTO error
+
+echo Cleaning Xilinx bitstreams
+docker run --rm -it --mac-address 00:01:02:03:04:05 -e TZ=America/Sao_Paulo -v %cd%:/workdir fbelavenuto/alteraquartus make -f Makefile-altera clean
 IF ERRORLEVEL 1 GOTO error
 
 goto ok
