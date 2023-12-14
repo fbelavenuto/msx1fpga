0d6e434d0d152453edd5cef5fc6241551f10edc8
Fabio Belavenuto <belavenuto@gmail.com>
2023-12-14 11:10:38 -0300

    Testing github actions with private builder

--- _build.cmd
+++ _build.cmd
@@ -9,6 +9,8 @@ echo Building software
 docker run --rm -it -e TZ=America/Sao_Paulo -v %cd%:/src fbelavenuto/8bitcompilers make -f Makefile-software
 IF ERRORLEVEL 1 GOTO error
 
+mkdir _BINs
+
 echo Building Xilinx FPGA bitstreams
 docker run --rm -it --mac-address 08:00:27:68:c9:35 -e TZ=America/Sao_Paulo -v %cd%:/workdir fbelavenuto/xilinxise make -f Makefile-xilinx
 IF ERRORLEVEL 1 GOTO error
